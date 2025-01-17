221be0277c7de524bd0bf05da88f261055785db7
c-harl-y <charlotte.schroeter@gmx.de>
2025-01-17 17:56:23 +0100

    aktualisiert

--- sketch.js
+++ sketch.js
@@ -1,11 +1,1 @@
-// Erstelle den Punkt-Cursor im DOM
-const cursor = document.createElement('div');
-cursor.classList.add('cursor');
-document.body.appendChild(cursor);
-
-// Bewegung des Punktes mit der Maus
-document.addEventListener('mousemove', (e) => {
-  cursor.style.left = e.clientX + 'px';
-  cursor.style.top = e.clientY + 'px';
-});
 
